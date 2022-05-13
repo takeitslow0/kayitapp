624e72daf8b848230a42a2d4a06d059151a57e0f
Yağız Yılmaz <yagizzyilmazz21@hotmail.com>
2022-05-13 15:45:15 +0300

    homework

---
+++ MvcWebUI/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+using Business.Services;
+using Business.Services.Bases;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddScoped<IKategoriService, KategoriService>();
+builder.Services.AddScoped<IUrunService, UrunService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ MvcWebUI/wwwroot/js/main.js
@@ -0,0 +1,51 @@
+﻿// withtout session or local storage
+//$(document).ready(function () {
+//    $('#sidebarCollapse').on('click', function (event) {
+//        event.preventDefault();
+//        $('#sidebar').toggleClass('active');
+//        var cssClass = $('#i-collapse').attr('class');
+//        if (cssClass.includes('left')) {
+//            $('#i-collapse').removeClass('fa-chevron-left');
+//            $('#i-collapse').addClass('fa-chevron-right');
+//            $('#sidebarCollapse').attr('title', 'Open Menu');
+//        } else {
+//            $('#i-collapse').removeClass('fa-chevron-right');
+//            $('#i-collapse').addClass('fa-chevron-left');
+//            $('#sidebarCollapse').attr('title', 'Close Menu');
+//        }
+//    });
+//});
+
+// with session or local storage
+$(document).ready(function () {
+    //var sideBar = localStorage.getItem("sidebar"); // tarayıcı kapandıktan sonra tarayıcı storage'ında tutulmaya devam eder
+    var sideBar = sessionStorage.getItem("sidebar"); // tarayıcı kapandıktan sonra tarayıcı storage'ından siler
+    if (sideBar == null) {
+        sideBar = "1";
+        sessionStorage.setItem("sidebar", "1");
+    }
+    if (sideBar === "1") {
+        $('#i-collapse').removeClass('fa-chevron-right');
+        $('#i-collapse').addClass('fa-chevron-left');
+        $('#sidebarCollapse').attr('title', 'Soldaki Menüyü Kapat');
+        $('#sidebar').removeAttr("style");
+        $('#sidebar').removeClass("active");
+    } else {
+        $('#i-collapse').removeClass('fa-chevron-left');
+        $('#i-collapse').addClass('fa-chevron-right');
+        $('#sidebarCollapse').attr('title', 'Soldaki Menüyü Aç');
+        $('#sidebar').css("display", "none");
+        $('#sidebar').addClass("active");
+    }
+    $('#sidebarCollapse').on('click', function (event) {
+        event.preventDefault();
+        sideBar = sessionStorage.getItem("sidebar");
+        if (sideBar === "1") {
+            sideBar = "0";
+        } else {
+            sideBar = "1";
+        }
+        sessionStorage.setItem("sidebar", sideBar);
+        window.location.reload(); // tarayıcıyı bulunduğu url'ye yönlendirir
+    });
+});
